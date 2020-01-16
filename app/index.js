e20574b9d09e378d4ce8c7f62ed6a209a6a753fe
Bhavin Sasapra <imbhavin95@gmail.com>
2020-01-16 23:27:27 +0530

    update code

---
+++ app/index.js
@@ -0,0 +1,1 @@
+console.log('Bhavin New Sasapra');
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,12 @@
-var path = require('path');
-
 module.exports = {
-    entry: './app/index.js',
+    entry: __dirname + '/app/index.js',
     output: {
-        path: __dirname + './dist',
+        path: __dirname + '/dist',
         filename: 'bundle.js',
         publicPath: '/'
     },
-    module: {
-      rules: [
-          {
-            test: /.js$/,
-            exclude:/(node_modules)/,
-            // include: path.resolve(__dirname, 'src'), (If we need to apply in particular directory)
-            use:{
-                loader: 'babel-loader',
-                options:{
-                    presents: ['es2015']
-                }
-            }
-        },
-        { test: /.css/, use: ['style-loader', 'css-loader'] }
-      ]
-    }
+    devServer: {
+        inline: false,
+        contentBase: "./dist",
+    },
 }
