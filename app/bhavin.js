2d227f111acc9ab63518cf3e48f7e28c2569b2c0
Bhavin Sasapra <imbhavin95@gmail.com>
2020-01-20 01:49:05 +0530

    final demo

---
+++ app/bhavin.js
@@ -0,0 +1,3 @@
+var name = 'Bhavin Sasapra';
+
+module.exports = name;
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,24 @@ module.exports = {
         filename: 'bundle.js',
         publicPath: '/'
     },
-    devServer: {
-        inline: false,
-        contentBase: "./dist",
-    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
+    }
+
 }
